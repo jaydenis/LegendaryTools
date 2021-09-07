638121723c3052f0b00645a25ae2fa6bc025f190
jay.tech66 <jay.tech66@gmail.com>
2021-09-07 11:31:40 -0400

    v0.1.8.236

--- CustomSetBuilder/Properties/Resources.Designer.cs
+++ CustomSetBuilder/Properties/Resources.Designer.cs
@@ -123,26 +123,6 @@ namespace CustomSetBuilder.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap CardBack {
-            get {
-                object obj = ResourceManager.GetObject("CardBack", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap CardBack1 {
-            get {
-                object obj = ResourceManager.GetObject("CardBack1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap download {
             get {
                 object obj = ResourceManager.GetObject("download", resourceCulture);
